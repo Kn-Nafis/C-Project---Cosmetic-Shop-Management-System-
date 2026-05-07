6a9dbcc625ede6a6507a54c9b9543bd787896290
First Git <knrnafis1@gamil.com>
2026-05-07 13:35:39 +0600

    hi

--- Cosmatic Shop Management/Program.cs
+++ Cosmatic Shop Management/Program.cs
@@ -12,7 +12,7 @@ namespace Cosmatic_Shop_Management
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-            Application.Run(new MyOrdersForm());
+            Application.Run(new ReviewForm());
         }
     }
 }
